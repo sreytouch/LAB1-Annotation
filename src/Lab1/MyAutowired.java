f77e2ba19c7e4c48aee4a38cd9a49f286f4db22e
sreytouch <atouch.it@gmail.com>
2022-06-22 08:32:22 -0500

    annotation

---
+++ src/Lab1/MyAutowired.java
@@ -0,0 +1,14 @@
+package Lab1;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD) 
+//type is class
+
+public @interface MyAutowired {
+	
+}
---
+++ src/Lab1/MyInjector.java
@@ -0,0 +1,64 @@
+package Lab1;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@MyBean
+public class MyInjector {
+	@MyAutowired
+	private Map<Class, Object> container = new HashMap<>();
+
+	public Set<Class> findAllClassesUsingClassLoader(String name) {
+		InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream(name);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		return reader.lines().filter(line -> line.endsWith(".class")).map(line -> getClass(line, name))
+				.collect(Collectors.toSet());
+	}
+
+	private Class getClass(String className, String packageName) {
+		try {
+			return Class.forName(packageName + "." + className.substring(0, className.lastIndexOf('.')));
+		} catch (ClassNotFoundException e) {
+			// handle the exception
+		}
+		return null;
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
+			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
+		MyInjector s = new MyInjector();
+		String myclass = s.getClass().getPackageName();
+		System.out.println(myclass);
+		// System.out.println(s.findAllClassesUsingClassLoader(myclass));
+
+		for (Class c : s.findAllClassesUsingClassLoader(myclass)) {
+			System.out.println(c.getSimpleName());
+
+//		Class<?> m = Class.forName(myclass +"."+c.getSimpleName());
+//		Constructor<?> ctor  =  m.getConstructor(String.class);
+//		Object object = ctor.newInstance("Test");
+//		System.out.println(object);
+			// System.out.println("HERE
+			// "+c.getClass().isAnnotationPresent(MyAutowired.class));
+
+		}
+	}
+
+	public void scanForBeans() {
+		// search
+		// put to map
+	}
+
+	public void scanForAutowired() {
+		// search
+		// put to map
+	}
+
+}
